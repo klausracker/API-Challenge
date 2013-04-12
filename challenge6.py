deab228554edc9323e8c9f6fc6d703e8bb58cdb7
Klaus Von Racker <kevin.hinze@rackspace.com>
2013-04-11 20:54:20 -0500

    challenge6.py

---
+++ challenge6.py
@@ -0,0 +1,28 @@
+import pyrax
+import sys
+
+pyrax.set_credential_file("~/.rackspace_cloud_credentials")
+
+cf = pyrax.cloudfiles
+
+def main():
+  createcont()
+
+def createcont():
+  print "Hello, about to create a CDN enabled container."
+  print
+  contname = str(raw_input ("What would you like to call this container? "))
+  cont = cf.create_container(contname)
+  print "Creating container ", cont.name, "if it doesn't already exist."
+  print "If it does exist, we'll just enable the existing container."
+  print
+  ttl = str(raw_input ("What TTL value would you like to set for this public container? ")) 
+  enablecont(cont, contname, ttl)
+
+def enablecont(cont, contname, ttl):
+  cont = cf.make_container_public(cont.name, ttl=ttl)
+  print
+  print "Done!"
+
+if __name__ == "__main__":
+  main()
